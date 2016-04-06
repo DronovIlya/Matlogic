1cd411495564ab397344fe4ff3c5529ee266f6cf
Ilya Dronov <ilya.dronov@corp.mail.ru>
2016-04-07 02:21:56 +0300

    task6 stable

--- task6/primitives.h
+++ task6/primitives.h
@@ -1,11 +1,15 @@
 #ifndef PRIMITIVES
 #define PRIMITIVES
 
-struct Z {
+#include <vector>
 
+struct Z {
     static const int size = 1;
 	static int eval(std::vector<int> &args)
 	{
+        if (args.size() != 1) {
+            std::cout << "Z function must have 1 argument" << std::endl;
+        }
 		return 0;
 	}
 };
@@ -15,6 +19,9 @@ struct N
     static const int size = 1;
 	static int eval(std::vector<int> &args)
 	{
+        if (args.size() != 1) {
+            std::cout << "N function must have 1 argument" << std::endl;
+        }
 		return args[0] + 1;
 	}	
 };
@@ -44,43 +51,91 @@ template<typename f, typename g>
 struct R
 {
     static const int size = f::size;
-    static int eval(std::vector<int> const & args)
+    static int eval(std::vector<int> & args)
     {
-        std::vector<int> p (args.begin(), --args.end());
-
-		// for (int i = 0; i < args.size(); i++) {
-		// 	printf("%d ", args[i]);
-		// }
-		// printf("\n");
-
-        if (args.back() == 0)
+        int y = 0;
+        int count = args[args.size() - 1];
+        args.pop_back();
+        int res = f::eval(args);
+        args.push_back(0);
+        args.push_back(res);
+        for (int i = 0; i < count; i++)
         {
-            return f::eval(p);
+            res = g::eval(args);
+            args[args.size() - 2]++;
+            args[args.size() - 1] = res;
         }
-        else
-        {
-            p.push_back(args.back()-1);
-            int res = R<f, g>::eval(p);
-            p.push_back(res);
-            return g::eval(p);
-        }
-
+        args.pop_back();
+        args.pop_back();
+        args.push_back(count);
+        return res;
     }
 };
 
 template<typename f>
 struct M {
     static const int size = f::size - 1;
-    static int eval(std::vector<int> const & args)
+    static int eval(std::vector<int> & args)
     {
-        int y=0;
-        std::vector<int> p = args;
-        p.push_back(y);
-        while(f::eval(p) != 0) {
-            p[size] = ++y;
+        int i = 0;
+        args.push_back(0);
+        while (true)
+        {
+            if (f::eval(args) == 0) {
+                args.pop_back();
+                return i;
+            }
+            i++;
+            args[args.size() - 1]++;
         }
-        return y;
+        return i;
     }
 };
 
-#endif PRIMITIVES
+// --a
+typedef S<R<Z, U<3,2>>, U<1,1>, U<1,1>> dec; 
+// a + b
+typedef R<U<1, 1>, S<N, U<3, 3>>> add;
+// a - b
+typedef R<U<1,1>, S<dec, U<3, 3>>> substract;
+// a * b
+typedef R<Z, S<add, U<3,1>, U<3,3>>> mul;
+// a / b
+typedef S<dec, S<M<S<substract, U<3, 1>, S<mul, U<3, 2>, U<3, 3>>>>, S<N, U<2, 1>>, U<2, 2>>> divide;
+// a % b
+typedef S<substract, U<2,1>, S<mul, U<2,2>, divide>> mod;
+// a ^ b
+typedef R<S<N,Z>, S<mul, U<3,1>, U<3,3>>> pow;
+
+// statements
+typedef S<R<S<N, Z>,S<Z, U<3, 2>>>, U<1, 1>, U<1, 1>> is_zero;
+typedef S<substract, S<N,Z>, is_zero> is_not_zero;
+
+typedef S<is_zero, substract> less_eq;
+typedef S<R<U<2, 1>, U<4, 2>>, U<2,1>, U<2,2>, S<is_zero, U<2,1>>> non_zero;
+
+// minimum
+typedef S<R<U<2,2>, U<4,1>>, U<2,1>, U<2,2>, less_eq> min;
+// maximum
+typedef S<substract, add, min> max;
+
+// primitive numbers
+typedef S<N, Z> one;
+typedef S<N, S<N, Z>> two;
+
+// workaround with prime numbers
+typedef S<R<Z, S<non_zero, U<3, 3>, S<is_zero, S<mod, U<3, 1>, S<add, U<3, 2>, 
+    S<two, U<3, 1>>>>>>>, U<1, 1>, S<substract, U<1,1>, two>> _is_prime;
+
+typedef S<R<S<substract, S<N, Z>, _is_prime>, S<Z, U<3, 1>>>, U<1, 1>, 
+    S<less_eq, U<1, 1>, S<N, Z>>> is_prime;
+
+typedef S<R<Z, S<add, U<3, 3>, S<is_prime, U<3, 2>>>>, U<1, 1>, S<N, U<1, 1>>> count_primes; 
+// calculate n-th prime number
+typedef M<S<substract, S<N, U<2,1>>, S<count_primes, U<2,2>>>> nth_prime;
+
+// partial log
+typedef S<R<S<Z,U<2,1>>, S<max, U<4,4>, S<R<S<Z, U<1,1>>, U<3,1>>, U<4,3>, S<is_zero, S<mod, U<4,2>, 
+    S<pow, U<4,1>, U<4,3>>>>>>>, U<2,1>, U<2,2>, U<2,2>> plog;
+
+#endif
--- task6/task6_main.cpp
+++ task6/task6_main.cpp
@@ -1,108 +1,120 @@
 #include <cstdio>
 #include <iostream>
+#include <time.h>
 #include <vector>
 #include "primitives.h"
 
-typedef S<N, Z> one;
-typedef R< U<1,1>, S<N, U<3,3> > > sum;
-typedef S< R<Z, U<3,2> >, U<1,1>, U<1,1> > minus_1;
-typedef R< U<1,1>, S<minus_1, U<3,3> > > sub;
-typedef R< Z, S<sum, U<3,1>, U<3,3> > > mul;
-typedef R<one, S<mul, U<3,1>, U<3,3> > > pow;
-typedef S< minus_1, S< M< S<sub, U<3,1>, S<mul, U<3, 2>, U<3, 3> > > >, S<N, U<2, 1> >, U<2, 2> > > divide;
-typedef S<sub, U<2,1>, S<mul, U<2,2>, S< divide, U<2,1>, U<2,2> > > > mod;
-typedef S< R< N, S<Z, U<3, 3> > >, U<1,1>, U<1,1> > _not;
-typedef S<_not, mod> divideable;
-typedef S< minus_1, M<S< divideable, U<3,2>, S<pow, U<3,1>, U<3,3> > > > > plog;
-typedef _not is_zero;
-typedef S<_not, is_zero> is_not_zero;
-typedef S<is_not_zero, mul> _and;
-typedef S<N, N> plus2;
-typedef S< is_not_zero, sub> greater;
-typedef S<N, one> two;
-typedef S<
-    R<
-        S<greater, U<1,1>, one>,
-        S<
-            S< _and,
-                S< mod, U<3,1>, U<3,2> >,
-                U<3,3>
-            >,
-            U<3,1>,
-            S<plus2, U<3,2> >,
-            U<3,3>
-        >
-    >,
-    U<1,1>,
-    S<
-        minus_1,
-        S<
-            divide,
-            U<1,1>,
-            two
-        >
-    >
-> is_prime;
-
-
-typedef S<
-    R<
-        Z,
-        S<
-            sum,
-            S<is_prime, U<3,2> >,
-            U<3,3>
-        >
-    >,
-    U<1,1>,
-    S<
-        N,
-        U<1,1>
-    >
-> prev_prime;
-
-
-typedef S <
-    M<
-        S<
-            greater,
-            U<2,1>,
-            S<prev_prime, U<2,2> >
-        >
-    >,
-    N
-> nth_prime;
-
+#define MAX_TEST_COUNT_PER_FUNCTION 5
 
 template<typename f, typename... g>
-void run(g... args)
+int run(g... args)
 {
 	std::vector<int> result {args...};
-	std::cout << f::eval(result) << std::endl;
+	return f::eval(result);
 }
 
-typedef Z zero;
-typedef R<U<1,1>,S<N, U<3,3>>> add;
+void testAdd()
+{
+	printf("### ADD tests start processing... ###\n");
+	int success_count = 0;
+	for (int i = 0; i < MAX_TEST_COUNT_PER_FUNCTION; i++) {
+		std::vector<int> args = {rand() % 200, rand() % 200};
+		
+		int actual = run<add>(args[0], args[1]);
+		int expected = args[0] + args[1];
+
+		printf("a = %d, b = %d, actual = %d, expected = %d\n", args[0], args[1],
+			actual, expected);
+		success_count += (actual == expected ? 1 : 0);
+	}
+	printf("result = %d/%d\ncorrectness = %s\n", success_count, MAX_TEST_COUNT_PER_FUNCTION, 
+		success_count == MAX_TEST_COUNT_PER_FUNCTION ? "OK" : "FAIL");
+	printf("### ADD tests end processing... ###\n");
+}
+
+void testSubstract()
+{
+	printf("### SUBSTRACT tests start processing... ###\n");
+	int success_count = 0;
+	for (int i = 0; i < MAX_TEST_COUNT_PER_FUNCTION; i++) {
+		std::vector<int> args = {rand() % 200, rand() % 200};
+		
+		int actual = run<substract>(args[0], args[1]);
+		int expected = args[0] - args[1];
+		expected = expected > 0 ? expected : 0;
+
+		printf("a = %d, b = %d, actual = %d, expected = %d\n", args[0], args[1],
+			actual, expected);
+		success_count += (actual == expected ? 1 : 0);
+	}
+	printf("result = %d/%d\ncorrectness = %s\n", success_count, MAX_TEST_COUNT_PER_FUNCTION, 
+		success_count == MAX_TEST_COUNT_PER_FUNCTION ? "OK" : "FAIL");
+	printf("### SUBSTRACT tests end processing... ###\n");
+}
+
+void testMul()
+{
+	printf("### MUL tests start processing... ###\n");
+	int success_count = 0;
+	for (int i = 0; i < MAX_TEST_COUNT_PER_FUNCTION; i++) {
+		std::vector<int> args = {rand() % 200, rand() % 200};
+		
+		int actual = run<mul>(args[0], args[1]);
+		int expected = args[0] * args[1];
+
+		printf("a = %d, b = %d, actual = %d, expected = %d\n", args[0], args[1],
+			actual, expected);
+		success_count += (actual == expected ? 1 : 0);
+	}
+	printf("result = %d/%d\ncorrectness = %s\n", success_count, MAX_TEST_COUNT_PER_FUNCTION, 
+		success_count == MAX_TEST_COUNT_PER_FUNCTION ? "OK" : "FAIL");
+	printf("### MUL tests end processing... ###\n");
+}
+
+void testIsPrime() {
+	printf("### IS_PRIME tests start processing... ###\n");
+	int success_count = 0;
+	for (int i = 0; i < MAX_TEST_COUNT_PER_FUNCTION; i++) {
+		std::vector<int> args = {rand() % 200};
+		
+		int actual = run<is_prime>(args[0]);
+
+		bool ok = true;
+		for (int i = 2; i < args[0]; i++)
+			if (args[0] % i == 0) {
+				ok = false;
+			}
+		int expected = ok ? 1 : 0;
+
+		printf("a = %d, b = %d, actual = %d, expected = %d\n", args[0], args[1],
+			actual, expected);
+		success_count += (actual == expected ? 1 : 0);
+	}
+	printf("result = %d/%d\ncorrectness = %s\n", success_count, MAX_TEST_COUNT_PER_FUNCTION, 
+		success_count == MAX_TEST_COUNT_PER_FUNCTION ? "OK" : "FAIL");
+	printf("### IS_PRIME tests end processing... ###\n");
+
+}
+
+void testNthPrime() {
+	printf("### IS_PRIME tests start processing... ###\n");
+	int success_count = 0;
+	for (int i = 0; i < 10; i++) {
+		printf("%d-th prime number is = %d\n", (i + 1), run<nth_prime>(i));
+	}
+	printf("### IS_PRIME tests end processing... ###\n");
+
+}
 
 
 int main()
 {
-	run<one>();
-    run<sum>(120,100);
-    run<minus_1>(120);
-    run<sub>(120,100);
-    run<mul>(120,100);
-    run<pow>(2,5);
-    run<divide>(8,3);
-    run<mod>(8,3);
-    run<_not>(1);
-    run<divideable>(6,3);//нацело
-    run<plog>(3,12);
-    run<is_zero>(0);
-    run<_and>(1,4);//isnotzero(mul)
-    run<greater>(5,1);//first > second
-    run<two>(1000);//return 2
-    run<is_prime>(23);
-	run<nth_prime>(8);
+	printf("--- tests start processing... ---\n");
+	testAdd();
+	testSubstract();
+	testMul();
+	testIsPrime();
+	testNthPrime();
+	printf("--- tests end processing... ---\n");
 	return 0;
 }
